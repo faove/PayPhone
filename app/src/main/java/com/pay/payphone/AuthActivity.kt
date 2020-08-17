d00b9fa13947486f9d93ba6e52814f5f9b2b0ae2
Francisco Alvarez <falvarez@bd360.online>
2020-08-17 00:10:18 -0300

    splashscreen

--- app/src/main/java/com/pay/payphone/AuthActivity.kt
+++ app/src/main/java/com/pay/payphone/AuthActivity.kt
@@ -16,6 +16,7 @@ class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        Thread.sleep(2000)
         setTheme(R.style.AppTheme)
 
         super.onCreate(savedInstanceState)
